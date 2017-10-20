f34d086cebb91e9402e3a2c1c8efc4cee199d747
u1 <chernykh.ds@yandex.ru>
2017-10-21 00:44:04 +0300

    remove inject css code

--- inject.js
+++ inject.js
@@ -14,15 +14,6 @@ function showspeed() {
     this.parentElement.insertBefore(fragment, this.parentElement.firstChild);
     this.classList.add('ytpbsi-init');
 
-    if (! document.body.classList.contains('ytpbsi-init')) {
-        var link = document.createElement('link');
-        link.href = chrome.runtime.getURL('inject.css');
-        link.type = 'text/css';
-        link.rel = 'stylesheet';
-        document.head.appendChild(link);
-        document.body.classList.add('ytpbsi-init');
-    }
-
 }
 
 var p = document.getElementsByTagName('video')[0];
